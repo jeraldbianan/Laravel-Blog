d474ed347cabc8f3ad2faba67295d380e6f83dbf
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 11:30:14 +0800

    setup posts and comments migrations

--- database/migrations/2024_10_10_032317_create_posts_table.php
+++ database/migrations/2024_10_10_032317_create_posts_table.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -20,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('posts');
     }
 };
--- database/migrations/2024_10_10_032714_create_comments_table.php
+++ database/migrations/2024_10_10_032714_create_comments_table.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Post::class)->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -20,8 +21,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('comments');
     }
 };
