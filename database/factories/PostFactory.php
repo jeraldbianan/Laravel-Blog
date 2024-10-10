4a3409b691071ad9a0e457e544221941de727ef5
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 11:34:59 +0800

    setup comments and posts factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,17 +7,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
-class PostFactory extends Factory
-{
+class PostFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            //
+            'title' => fake()->sentence,
+            'body' => fake()->sentence(40)
         ];
     }
 }
