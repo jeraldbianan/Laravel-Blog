212fe33afef5ecdb89098e98b096006b1b7afa99
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 10:23:18 +0800

    setup view and routes for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
