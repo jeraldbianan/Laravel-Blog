a8974742e65aace1154d3f0c19067e6c39756ed0
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 10:34:04 +0800

    importing css and js dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
-import './bootstrap';
+import "./bootstrap";
+
+import "../admin/js/datatables-simple-demo";
+import "../admin/js/scripts";
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,6 +11,8 @@
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body class="sb-nav-fixed">
