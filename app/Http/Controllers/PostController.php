c2eff02cd771657c7e22737ffeb51088064ad0c9
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 13:15:26 +0800

    used eager loading in displaying posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller {
      * Display a listing of the resource.
      */
     public function index() {
-        $posts = Post::all();
+        // $posts = Post::all();
+        $posts = Post::with(['comments', 'user'])->get();
 
         return view('admin.posts.index', compact('posts'));
     }
