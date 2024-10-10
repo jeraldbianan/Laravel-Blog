164f8e9bf3c308d38be6fcb2d0671cf14af696ab
Jerald Bianan <jeraldbianan@gmail.com>
2024-10-10 11:35:24 +0800

    build relationship of User->Post and Post->Comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Post extends Model
-{
+class Post extends Model {
     use HasFactory;
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -38,11 +38,14 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(): HasMany {
+        return $this->hasMany(Post::class);
     }
 }
